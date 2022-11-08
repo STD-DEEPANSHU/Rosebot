09bdffa2a5708874c0e8839459b32b18df242cd5
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2022-11-08 21:52:05 +0530

    Update __init__.py

--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 from tg_bot import DB_URI
 
+if DB_URI and DB_URI.startswith("postgres://"):
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
